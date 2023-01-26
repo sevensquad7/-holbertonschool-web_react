1bc352f4ed50c198c5fe5bee3ff7927504a488b1
sevensquad7 <edgarxot@gmail.com>
2023-01-26 12:35:43 -0500

    load files task_2

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
 		path: path.resolve(__dirname, 'public'),
 		filename: 'bundle.js',
 	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
 	mode: 'production',
 };
